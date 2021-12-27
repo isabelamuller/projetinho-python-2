6bcdf89f64c6853a60e5ff939ea8f4444c2d037a
Isabela Müller <isabelamuller1@hotmail.com>
2021-12-27 02:23:06 -0300

    NOSSA N CREIO Q DEU CERTOOO #feliz

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
-from PIL import ImageTk, Image
-from tkinter import filedialog
-import os
 
 root = tk.Tk()
-root.geometry('495x450')
+root.geometry('405x600')
 root.title('guess the flag')
-Label(root, text='can you guess these five european flags?', font='courier 20 bold').grid()
+Label(root, text='can you guess this european flag?', font='courier 20 bold').grid()
 Label(root, text='good luck!', font='courier 15').grid()
-# buttonSt = tk.Button(root, text='start', command=france)
-# buttonSt.grid()
-canvas = Canvas(root, width=300, height=230)
-canvas.grid()
-img = (Image.open("france.svg.png"))
-resized_image = img.resize((300, 205), Image.ANTIALIAS)
-new_image = ImageTk.PhotoImage(resized_image)
-canvas.create_image(20, 20, anchor=NW, image=new_image)
 pais1_var = tk.StringVar()
-pais2_var = tk.StringVar()
-pais3_var = tk.StringVar()
-pais4_var = tk.StringVar()
-pais5_var = tk.StringVar()
+
+
+def buttonst():
+    canvas1 = Canvas(root, width=350, height=233)
+    canvas1.grid(row=3, column=0)
+    img2 = PhotoImage(file="france.svg.png")
+    canvas1.create_image(0, 0, image=img2, anchor=NW)
+    canvas1.img2 = img2
+
+
+buttonst1 = tk.Button(root, text='start', command=buttonst)
+buttonst1.grid(row=2, column=0)
 
 
 def france():
@@ -31,17 +28,6 @@ def france():
     if pais == "france":
         tk.messagebox.showinfo("congrats!", "congrats! you got it right! :)")
 
-
-    else:
-        tk.messagebox.showerror("oh no!", "try again baby")
-
-
-def sweden():
-    pais = pais2_var.get()
-
-    if pais == "sweden":
-        tk.messagebox.showinfo("congrats!", "congrats! you got it right once again! :)")
-
     else:
         tk.messagebox.showerror("oh no!", "try again baby")
 
@@ -49,9 +35,22 @@ def sweden():
 pais_label = tk.Label(root)
 pais_entry = tk.Entry(root, textvariable=pais1_var)
 
-pais_label.grid(row=3, column=0)
-pais_entry.grid(row=3, column=0)
+pais_label.grid(row=4, column=0)
+pais_entry.grid(row=4, column=0)
 sub_btn = tk.Button(root, text='submit', command=france)
+sub_btn.grid(row=7, column=0)
+
+
+def buttonnx():
+    canvas2 = Canvas(root, width=150, height=150)
+    canvas2.grid(row=9, column=0)
+    img1 = PhotoImage(file="surprise.png")
+    canvas2.create_image(0, 0, image=img1, anchor=NW)
+    canvas2.img1 = img1
+
+
+buttonNt = Button(root, text='surprise', command=buttonnx)
+buttonNt.grid(row=8, column=0)
 
 
 def ExitApp():
@@ -62,7 +61,6 @@ def ExitApp():
         tk.messagebox.showinfo('welcome back')
 
 
-sub_btn.grid(row=4, column=0)
 buttonEg = tk.Button(root, text='exit', command=ExitApp)
-buttonEg.grid()
+buttonEg.grid(row=10, column=0)
 root.mainloop()
